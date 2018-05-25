0450fa5ae38f9648daa0ae1e2c5aa743a3d7748c
ronaldo faustino <ohnirucse@msn.com>
2018-05-25 18:14:45 -0300

    Test Completo2

--- CursoSelenium/src/main/java/TestAlert.java
+++ CursoSelenium/src/main/java/TestAlert.java
@@ -1,3 +1,7 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,4 +12,61 @@ public class TestAlert {
 		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
 		
 	}
+	@Test
+	
+	public void deveInteragirComAlertSimples(){
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
+		driver.findElement(By.id("alert")).click();
+		Alert alert = driver.switchTo().alert();
+		String texto = alert.getText();
+		Assert.assertEquals("Alert Simples", alert.getText());
+		alert.accept();
+		driver.findElement(By.name("elementosForm:nome")).sendKeys(texto);
+	}
+	
+	@Test
+	
+	public void deveInteragirComAlertConfirme(){
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
+		driver.findElement(By.id("confirm")).click();
+		Alert alerta = driver.switchTo().alert();
+		String texto = alerta.getText();
+		Assert.assertEquals("Confirm Simples", alerta.getText());
+		alerta.accept();
+		Assert.assertEquals("Confirmado", alerta.getText());
+		alerta.accept();
+		
+		driver.findElement(By.id("confirm")).click();
+		alerta = driver.switchTo().alert();
+		Assert.assertEquals("Confirm Simples", alerta.getText());
+		alerta.dismiss();
+		Assert.assertEquals("Negado", alerta.getText());
+		alerta.accept();
+	}
+	
+@Test
+	
+	public void deveInteragirComPront(){
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
+		driver.findElement(By.id("prompt")).click();
+		Alert alerta = driver.switchTo().alert();
+		Assert.assertEquals("Digite um numero", alerta.getText());
+		alerta.sendKeys("1245");
+		alerta.accept();
+		Assert.assertEquals("Era 1245?", alerta.getText());
+		alerta.accept();
+		Assert.assertEquals(":D", alerta.getText());
+		alerta.accept();
+		
+		
+ 	}
 }
